5d08037dd72d5af9d83a4579d959c7e092809826
SoExDobin <56966606+SoexDobin@users.noreply.github.com>
2026-01-09 19:49:44 +0900

    Fix error logging in notion-sync.js

--- .github/scripts/notion-sync.js
+++ .github/scripts/notion-sync.js
@@ -77,7 +77,8 @@ async function run() {
     else if (eventName === "pull_request") await syncPR();
   } catch (error) {
     // 여기서 출력되는 에러 메시지가 가장 중요합니다.
-    console.error("동기화 에러 발생 상세 내역:");
+    console.log(eventName);
+    console.error(동기화 에러 발생 상세 내역:");
     console.error(error.body || error);
     process.exit(1);
   }
